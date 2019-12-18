5b93520436d1e9d9299d90b1112945a2071bdc08
Sam Barrett <samuelbarrett1234@btinternet.com>
2019-12-18 07:45:31 +0000

    Added unit test project for ATPSearch.

---
+++ ATPSearchUnitTests/LockManagerTests.cpp
@@ -0,0 +1,16 @@
+#include "Test.h"
+#include <LockManager.h>
+
+
+BOOST_AUTO_TEST_SUITE(LockManagerTests);
+
+
+BOOST_DATA_TEST_CASE(XLockBlockTest)
+{
+
+}
+
+
+BOOST_AUTO_TEST_SUITE_END();
+
+
---
+++ ATPSearchUnitTests/Test.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+// Author: Samuel Barrett
+
+
+#include <boost/test/unit_test.hpp>
+#include <boost/mpl/list.hpp>
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/data/monomorphic.hpp>
+
+

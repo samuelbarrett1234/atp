59ea7bfc68b57bd67c1295fd4494eb02d7b6a04a
Sam Barrett <samuelbarrett1234@btinternet.com>
2020-04-17 07:51:27 +0100

    Implemented a free variable id set.

---
+++ ATPLogic/Internal/FreeVarIdSet.h
@@ -0,0 +1,289 @@
+#pragma once
+
+
+/**
+\file
+
+\author Samuel Barrett
+
+\brief Contains a `FreeVarIdSet` data structure for holding sets of
+	free variable IDs.
+*/
+
+
+#include <algorithm>
+#include <boost/container/small_vector.hpp>
+#include "../ATPLogicAPI.h"
+
+
+namespace atp
+{
+namespace logic
+{
+
+
+/**
+\brief This class represents a set of free variable IDs
+
+\details Why not just use std::set<size_t>? Because this class takes
+	advantage of the fact that free variable IDs are usually close
+	together (dense), so we subtract the minimum ID from all of them,
+	and then store it as a bitmap.
+
+\note Don't need to export this class as it is fully inlined.
+*/
+class FreeVarIdSet
+{
+public:
+	template<typename _ValTy,
+		typename _PtrTy, typename _RefTy,
+		typename _ParentTy>
+	class _iterator
+	{
+		friend FreeVarIdSet;
+	private:
+		typedef _iterator<_ValTy, _PtrTy, _RefTy,
+			_ParentTy> _my_iter_ty;
+	public:
+		// conforming to the standard iterator template interface
+
+		typedef std::forward_iterator_tag iterator_category;
+		typedef _ValTy value_type;
+		typedef _RefTy reference;
+		typedef _PtrTy pointer;
+		typedef size_t difference_type;
+
+		inline _iterator(_ParentTy parent,
+			size_t idx) :
+			m_parent(parent), m_idx(idx)
+		{ }
+
+		inline _iterator(const _my_iter_ty& other) :
+			m_parent(other.m_parent),
+			m_idx(other.m_idx)
+		{ }
+
+		inline _iterator(_my_iter_ty&& other) noexcept :
+			m_parent(other.m_parent),
+			m_idx(other.m_idx)
+		{ }
+
+		inline _my_iter_ty& operator= (const _my_iter_ty& other)
+		{
+			m_parent = other.m_parent;
+			m_idx = other.m_idx;
+			return *this;
+		}
+
+		inline _my_iter_ty&
+			operator= (_my_iter_ty&& other) noexcept
+		{
+			m_parent = other.m_parent;
+			m_idx = other.m_idx;
+			return *this;
+		}
+
+		inline value_type operator*()
+		{
+			bring_forward();
+			return m_idx + m_parent->m_min_id;
+		}
+
+		pointer operator->() = delete;
+
+		inline _my_iter_ty& operator++()
+		{
+			bring_forward();
+
+			++m_idx;
+			return *this;
+		}
+
+		inline _my_iter_ty operator++(int)
+		{
+			iterator temp = *this;
+			++(*this);
+			return temp;
+		}
+
+		inline bool operator == (const _my_iter_ty& other) const
+		{
+			bring_forward();
+			other.bring_forward();
+
+			return (m_parent == other.m_parent &&
+				m_idx == other.m_idx);
+		}
+
+		inline bool operator != (const _my_iter_ty& other) const
+		{
+			return !(*this == other);
+		}
+
+	private:
+		void bring_forward() const
+		{
+			while (m_idx < m_parent->m_vec.size()
+				&& !m_parent->m_vec[m_idx])
+			{
+				++m_idx;
+			}
+		}
+
+	private:
+		_ParentTy m_parent;
+		mutable size_t m_idx;
+	};
+
+	typedef _iterator<size_t, size_t*,
+		size_t&, FreeVarIdSet*> iterator;
+	typedef _iterator<size_t, const size_t*,
+		const size_t&, const FreeVarIdSet*>
+		const_iterator;
+
+public:
+	FreeVarIdSet() :
+		m_min_id(0)
+	{ }
+
+	template<typename IterT>
+	FreeVarIdSet(IterT _begin, IterT _end) :
+		m_vec(std::distance(_begin, _end), false),
+		m_min_id((_begin != _end) ? *_begin : 0)
+	{
+		for (auto iter = _begin; iter != _end; ++iter)
+		{
+			m_vec[*iter - m_min_id] = true;
+		}
+	}
+
+	FreeVarIdSet(const FreeVarIdSet& other) :
+		m_vec(other.m_vec),
+		m_min_id(other.m_min_id)
+	{ }
+	FreeVarIdSet(FreeVarIdSet&& other) noexcept :
+		m_vec(std::move(other.m_vec)),
+		m_min_id(other.m_min_id)
+	{ }
+	FreeVarIdSet& operator= (const FreeVarIdSet& other)
+	{
+		m_vec = other.m_vec;
+		m_min_id = other.m_min_id;
+		return *this;
+	}
+	FreeVarIdSet& operator= (FreeVarIdSet&& other) noexcept
+	{
+		m_vec = std::move(other.m_vec);
+		m_min_id = other.m_min_id;
+		return *this;
+	}
+
+	inline void insert(size_t id)
+	{
+		if (m_vec.empty())
+		{
+			m_min_id = id;
+			m_vec.resize(1, true);
+		}
+		// three other cases (in two of these we have to resize
+		// m_vec)
+		else if (id < m_min_id)
+		{
+			// add elements to the beginning
+			const size_t extend_size = m_min_id - id;
+			m_min_id = id;
+			// extend with many elements at the back
+			m_vec.resize(m_vec.size() + extend_size, false);
+			// swap those new elements to the front
+			std::rotate(m_vec.rbegin(), m_vec.rbegin() + extend_size,
+				m_vec.rend());
+			m_vec[0] = true;  // set minimum to be true
+		}
+		else if (id >= m_min_id + m_vec.size())
+		{
+			const size_t extend_size =
+				id - m_min_id - m_vec.size() + 1;
+			m_vec.resize(m_vec.size() + extend_size, false);
+			m_vec.back() = true;
+		}
+		else
+		{
+			m_vec[id - m_min_id] = true;
+		}
+	}
+
+	inline bool contains(size_t id) const
+	{
+		if (id < m_min_id)
+			return false;
+		else if (id >= m_min_id + m_vec.size())
+			return false;
+		else return m_vec[id - m_min_id];
+	}
+
+	void erase(size_t id)
+	{
+		if (id >= m_min_id && id < m_vec.size() + m_min_id)
+		{
+			m_vec[id - m_min_id] = false;
+		}
+	}
+	void erase(iterator iter)
+	{
+		iter.bring_forward();
+		m_vec[iter.m_idx] = false;
+	}
+	void erase(const_iterator iter)
+	{
+		iter.bring_forward();
+		m_vec[iter.m_idx] = false;
+	}
+
+	inline size_t size() const
+	{
+		return std::count(m_vec.begin(), m_vec.end(), true);
+	}
+	inline size_t max() const
+	{
+		ATP_LOGIC_PRECOND(!empty());
+		auto iter = std::find(m_vec.rbegin(), m_vec.rend(),
+			true);
+		ATP_LOGIC_ASSERT(iter != m_vec.rend());
+		return m_min_id + std::distance(m_vec.begin(),
+			iter.base()) - 1;
+	}
+	inline bool empty() const
+	{
+		return std::none_of(m_vec.begin(), m_vec.end(),
+			[](bool b) { return b; });
+	}
+
+	inline iterator begin()
+	{
+		return iterator(this, 0);
+	}
+	inline iterator end()
+	{
+		return iterator(this, m_vec.size());
+	}
+	inline const_iterator begin() const
+	{
+		return const_iterator(this, 0);
+	}
+	inline const_iterator end() const
+	{
+		return const_iterator(this, m_vec.size());
+	}
+
+private:
+	// invariant: i is an ID in this set if and only if
+	// i >=  m_min_id and m_vec[i - m_min_id] is true
+	boost::container::small_vector<bool, 5> m_vec;
+	size_t m_min_id;
+};
+
+
+}  // namespace logic
+}  // namespace atp
+
+
---
+++ ATPLogicTests/FreeVarIdSetTests.cpp
@@ -0,0 +1,199 @@
+/**
+
+\file
+
+\author Samuel Barrett
+
+\brief This suite tests `atp::logic::FreeVarIdSet`
+
+*/
+
+
+#include <Internal/FreeVarIdSet.h>
+#include "Test.h"
+
+
+using atp::logic::FreeVarIdSet;
+
+
+struct FreeVarIdTestsFixture
+{
+	FreeVarIdSet set;
+};
+
+
+BOOST_FIXTURE_TEST_SUITE(FreeVarIdTests,
+	FreeVarIdTestsFixture);
+
+
+BOOST_DATA_TEST_CASE(basic_insert_test,
+	boost::unit_test::data::random(0, 10) ^
+	boost::unit_test::data::xrange(0, 100),
+	id, test_index)
+{
+	set.insert(id);
+	BOOST_TEST(set.contains(id));
+}
+
+
+BOOST_DATA_TEST_CASE(basic_insert_erase_test,
+	boost::unit_test::data::random(0, 10) ^
+	boost::unit_test::data::xrange(0, 100),
+	id, test_index)
+{
+	set.insert(id);
+	set.erase(id);
+	BOOST_TEST(!set.contains(id));
+}
+
+
+// the insertion of two IDs can be helpful for checking the three
+// cases in the insert/contains functions (see implementation for
+// which three cases are being referred to)
+BOOST_DATA_TEST_CASE(insert_two_test,
+	boost::unit_test::data::random(0, 10) ^
+	boost::unit_test::data::random(0, 10) ^
+	boost::unit_test::data::xrange(0, 100),
+	id1, id2, test_index)
+{
+	set.insert(id1);
+	set.insert(id2);
+
+	size_t min_id = std::min(id1, id2);
+	size_t max_id = std::max(id1, id2);
+	size_t middle_id = (id1 + id2) / 2;
+
+	// might wrap around, but this doesn't matter
+	BOOST_TEST(!set.contains(min_id - 1));
+	BOOST_TEST(!set.contains(max_id + 1));
+	// may or may not contain `middle_id`, depending on if it is
+	// equal to id1 or id2
+	BOOST_TEST(set.contains(middle_id) ==
+		(middle_id == id1 || middle_id == id2));
+}
+
+
+BOOST_AUTO_TEST_CASE(empty_test)
+{
+	BOOST_TEST(set.empty());
+	BOOST_TEST(set.size() == 0);
+	BOOST_TEST((set.begin() == set.end()));
+}
+
+
+BOOST_AUTO_TEST_CASE(many_test)
+{
+	size_t ids[5] =
+	{
+		1, 2, 3, 4, 7
+	};
+
+	for (size_t i : ids)
+	{
+		set.insert(i);
+	}
+
+	BOOST_TEST(set.size() == 5);
+	BOOST_TEST(!set.empty());
+	BOOST_TEST(std::distance(set.begin(), set.end()) == 5);
+
+	auto iter = set.begin();
+	for (size_t i = 0; i < 5; ++i, ++iter)
+	{
+		BOOST_TEST(*iter == ids[i]);
+		BOOST_TEST(set.contains(ids[i]));
+	}
+}
+
+
+BOOST_AUTO_TEST_CASE(max_test)
+{
+	size_t ids[5] =
+	{
+		0, 2, 3, 4, 7
+	};
+
+	for (size_t i : ids)
+	{
+		set.insert(i);
+	}
+
+	BOOST_TEST(set.max() == 7);
+}
+
+
+BOOST_AUTO_TEST_CASE(erase_out_of_range_test)
+{
+	set.insert(5);
+	set.insert(7);
+	set.erase(4);
+	set.erase(6);
+	set.erase(8);
+
+	BOOST_TEST(set.contains(5));
+	BOOST_TEST(set.contains(7));
+	BOOST_TEST(!set.contains(4));
+	BOOST_TEST(!set.contains(6));
+	BOOST_TEST(!set.contains(8));
+	BOOST_TEST(set.max() == 7);
+	BOOST_TEST(set.size() == 2);
+	BOOST_TEST(!set.empty());
+}
+
+
+BOOST_AUTO_TEST_CASE(test_erasing_using_iterators)
+{
+	set.insert(4);
+	set.insert(6);
+
+	auto iter = std::next(set.begin());
+
+	set.erase(iter);
+
+	BOOST_TEST(!set.contains(6));
+	BOOST_TEST(set.contains(4));
+	BOOST_TEST(set.size() == 1);
+	BOOST_TEST(!set.empty());
+	BOOST_TEST(set.max() == 4);
+}
+
+
+BOOST_AUTO_TEST_CASE(test_bring_forward)
+{
+	// in this case, the insertion of 0 forces the array to be bigger
+	set.insert(0);
+	set.insert(4);
+	set.insert(6);
+	set.erase(0);
+
+	auto iter = std::next(set.begin());
+
+	BOOST_TEST(*iter == 6);
+
+	set.erase(iter);
+	BOOST_TEST(!set.contains(6));
+	BOOST_TEST(set.contains(4));
+}
+
+
+BOOST_AUTO_TEST_CASE(test_iter_equality)
+{
+	// in this case, the insertion of 0 forces the array to be bigger
+	set.insert(0);
+	set.insert(4);
+	set.insert(6);
+	set.erase(0);
+
+	// test that, during equality testing, both iterators are
+	// brought forward
+
+	auto iter1 = std::next(set.begin());
+	auto iter2 = std::next(set.begin());
+
+	BOOST_TEST((iter1 == iter2));
+}
+
+
+BOOST_AUTO_TEST_SUITE_END();
+
+

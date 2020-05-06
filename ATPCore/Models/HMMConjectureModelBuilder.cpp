c31cf741e74da730afc1f0ec6a8ba44dcec88fa7
Sam Barrett <samuelbarrett1234@btinternet.com>
2020-05-06 07:09:22 +0100

    Developed HMM model builder.

---
+++ ATPCore/Models/HMMConjectureModelBuilder.cpp
@@ -0,0 +1,179 @@
+/**
+\file
+
+\author Samuel Barrett
+
+*/
+
+
+#include "HMMConjectureModelBuilder.h"
+
+
+namespace atp
+{
+namespace core
+{
+
+
+HMMConjectureModelBuilder::HMMConjectureModelBuilder(logic::ModelContextPtr p_ctx) :
+	m_ctx(std::move(p_ctx)),
+	m_valid(true)
+{
+	ATP_CORE_PRECOND(m_ctx != nullptr);
+}
+
+
+bool HMMConjectureModelBuilder::can_build() const
+{
+	return m_valid && m_q.has_value() && m_num_states.has_value()
+		&& check_state_trans() && check_obs();
+}
+
+
+void HMMConjectureModelBuilder::reset()
+{
+	m_valid = true;
+	m_state_trans.clear();
+	m_symb_obs.clear();
+	m_num_states = boost::none;
+	m_q = boost::none;
+}
+
+
+void HMMConjectureModelBuilder::add_symbol_observation(
+	size_t state, std::string symbol, float p)
+{
+	if (!m_ctx->is_defined(symbol))
+	{
+		// log this here, because even though this won't cause a
+		// problem (as this class is designed to be robust to such
+		// errors), there is nowhere else where we will have this
+		// kind of information (i.e. the specific symbol text).
+		ATP_CORE_LOG(warning) << "When building HMMConjectureModel, "
+			"encountered bogus symbol \"" << symbol << "\".";
+		m_valid = false;
+		return;
+	}
+
+	const size_t symb_id = m_ctx->symbol_id(std::move(symbol));
+
+	m_symb_obs[std::make_pair(state, symb_id)] = p;
+
+	m_valid = m_valid && p >= 0.0f && p <= 1.0f;
+}
+
+
+bool HMMConjectureModelBuilder::check_state_trans() const
+{
+	ATP_CORE_PRECOND(m_num_states.has_value());
+	ATP_CORE_PRECOND(m_valid);
+
+	// we need to check here that: between every two states there is
+	// a transition, and the sum of the probabilities coming out of
+	// a given state is 1
+
+	std::vector<std::vector<boost::optional<float>>> mat(
+		*m_num_states, std::vector<boost::optional<float>>(
+			*m_num_states, boost::none));
+
+	for (const auto& tr : m_state_trans)
+	{
+		const size_t pre = tr.first.first;
+		const size_t post = tr.first.second;
+		const float p = tr.second;
+
+		ATP_CORE_ASSERT(p >= 0.0f && p <= 1.0f);
+
+		if (pre >= *m_num_states || post >= *m_num_states)
+			return false;  // bad pre/post state
+
+		// the std::map ensures this
+		ATP_CORE_ASSERT(!mat[pre][post].has_value());
+
+		mat[pre][post] = p;
+	}
+
+	for (size_t i = 0; i < *m_num_states; ++i)
+	{
+		float sum = 0.0f;
+
+		for (size_t j = 0; j < *m_num_states; ++j)
+		{
+			// if state transition matrix hasn't been properly built
+			// then we are not in a position to build the HMM model!
+			if (!mat[i][j].has_value())
+				return false;
+
+			sum += *mat[i][j];
+		}
+
+		// if the state transition probabilities from state `i` don't
+		// sum to 1 (or, close enough) then we cannot build the model
+		if (std::abs(sum - 1.0f) > 1.0e-6f)
+			return false;
+	}
+
+	// if we get here, all checks have passed and we're good to go
+	return true;
+}
+
+
+bool HMMConjectureModelBuilder::check_obs() const
+{
+	ATP_CORE_PRECOND(m_num_states.has_value());
+	ATP_CORE_PRECOND(m_valid);
+
+	// only thing to check here is that: for every state, there is
+	// an entry to every symbol in the model context, and the sum of
+	// the probabilities for a state is <= 1 (raise a warning if
+	// exactly 1). We don't need equality with 1 because any residual
+	// probability is just the probability of generating a free
+	// variable.
+
+	// get all symbol IDs
+	auto all_symb_ids = m_ctx->all_constant_symbol_ids();
+	{
+		auto temp = m_ctx->all_function_symbol_ids();
+		all_symb_ids.insert(all_symb_ids.end(), temp.begin(),
+			temp.end());
+	}
+
+	for (size_t i = 0; i < *m_num_states; ++i)
+	{
+		float sum = 0.0f;
+
+		for (size_t id : all_symb_ids)
+		{
+			auto trans_iter = m_symb_obs.find(std::make_pair(i, id));
+
+			// we haven't covered all our bases
+			if (trans_iter == m_symb_obs.end())
+				return false;
+
+			sum += trans_iter->second;
+		}
+
+		if (sum > 1.0f)
+			return false;  // bad probabilities
+
+		if (sum == 1.0f)
+			ATP_CORE_LOG(warning) << "Encountered a HMM model which "
+				"had zero probability of generating a free variable";
+	}
+
+	// finally check that we aren't mapping any extra states
+	for (const auto& tr : m_symb_obs)
+	{
+		if (tr.first.first >= *m_num_states)
+			return false;  // bad state ID
+	}
+
+	// else all checks passed, so we're good
+	return true;
+}
+
+
+}  // namespace core
+}  // namespace atp
+
+
---
+++ ATPCore/Models/HMMConjectureModelBuilder.h
@@ -0,0 +1,147 @@
+#pragma once
+
+
+/**
+\file
+
+\author Samuel Barrett
+
+\brief Contains a class for building a HMMConjectureModel (which is
+	useful loading one from the database).
+
+*/
+
+
+#include <map>
+#include <boost/optional.hpp>
+#include <ATPLogic.h>
+#include "../ATPCoreAPI.h"
+
+
+namespace atp
+{
+namespace core
+{
+
+
+class HMMConjectureModel;  // forward declaration
+
+
+/**
+\brief A class for building a HMMConjectureModel out of tid-bits from
+	the database.
+
+\details This is to ensure that, once a model is obtained, it is
+	correct (invariants and preconditions satisfied).
+*/
+class ATP_CORE_API HMMConjectureModelBuilder
+{
+public:
+	HMMConjectureModelBuilder(logic::ModelContextPtr p_ctx);
+
+	/**
+	\brief Determine if the parameters that have been set so far are
+		valid.
+	*/
+	bool can_build() const;
+
+	/**
+	\brief Create a new HMMConjectureModel given the parameters
+
+	\pre can_build()
+
+	\returns A new HMMConjectureModel object
+	*/
+	std::unique_ptr<HMMConjectureModel> build() const;
+
+	/**
+	\brief Restore this object to the state it was when it was
+		constructed.
+	*/
+	void reset();
+
+	/**
+	\brief Set the number of hidden states. The states will then be
+		thought of as labelled 0..n-1 inclusive.
+	*/
+	void set_num_hidden_states(size_t n)
+	{
+		m_num_states = n;
+
+		m_valid = m_valid && *m_num_states > 0;
+	}
+
+	/**
+	\brief Set the fact that the system will transition from state
+		`pre` to state `post` with probability p
+	*/
+	void add_state_transition(size_t pre, size_t post, float p)
+	{
+		m_state_trans[std::make_pair(pre, post)] = p;
+
+		m_valid = m_valid && p >= 0.0f && p <= 1.0f;
+	}
+
+	/**
+	\brief Set the fact that, when in state `state`, we will observe
+		the symbol `symbol` with probability p.
+
+	\note This only applies to functions and constants, not to free
+		variables.
+	*/
+	void add_symbol_observation(
+		size_t state, std::string symbol, float p);
+
+	/**
+	\brief Set the parameter q=1-p for generating the free variable
+		IDs, where **p** (not q) is the parameter for the geometric
+		distribution.
+	*/
+	inline void set_free_geometric_q(float q)
+	{
+		m_q = q;
+
+		m_valid = m_valid && m_q >= 0.0f && m_q <= 1.0f;
+	}
+
+private:
+	/**
+	\brief Perform validity checking on state transitions
+
+	\pre m_num_states.has_value() && m_valid
+
+	\returns True iff m_state_trans represents a correct state
+		transition matrix (and of the correct size)
+	*/
+	bool check_state_trans() const;
+
+	/**
+	\brief Perform validity checking on observation matrix
+
+	\pre m_num_states.has_value() && m_valid
+
+	\returns True iff m_symb_obs represents a correct observation
+		matrix (and of the correct size), of course excluding free
+		variables.
+	*/
+	bool check_obs() const;
+
+private:
+	logic::ModelContextPtr m_ctx;
+
+	bool m_valid;  // check validity as we go along
+	// invariant: m_valid is true iff all the probabilities contained
+	// herein are between 0 and 1, and all symbol IDs are valid,
+	// and that we have a nonzero number of hidden states.
+
+	boost::optional<float> m_q;
+	boost::optional<size_t> m_num_states;
+	std::map<std::pair<size_t, size_t>, float> m_state_trans,
+		m_symb_obs;
+};
+
+
+}  // namespace core
+}  // namespace atp
+
+

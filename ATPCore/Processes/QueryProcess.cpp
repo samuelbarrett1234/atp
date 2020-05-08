52e9a4eb7bb19155d4e95b0386a5c41f70962424
Sam Barrett <samuelbarrett1234@btinternet.com>
2020-05-08 07:18:13 +0100

    Created QueryProcess which will help create processes which are centred around the execution of a query.

---
+++ ATPCore/Processes/QueryProcess.cpp
@@ -0,0 +1,96 @@
+/**
+\file
+
+\author Samuel Barrett
+
+*/
+
+
+#include "QueryProcess.h"
+
+
+namespace atp
+{
+namespace core
+{
+
+
+QueryProcess::QueryProcess(const db::DatabasePtr& p_db) :
+	m_done(false),
+	m_db(p_db)
+{
+}
+
+
+void QueryProcess::run_step()
+{
+	ATP_CORE_PRECOND(!m_done);
+	ATP_CORE_PRECOND(!waiting());
+
+	if (m_db_op)
+	{
+		switch (m_db_op->state())
+		{
+		case db::TransactionState::RUNNING:
+		{
+			auto p_query = dynamic_cast<db::IQueryTransaction*>(
+				m_db_op.get());
+			if (p_query != nullptr && p_query->has_values())
+			{
+				ATP_CORE_LOG(debug) << "Extracting values from query...";
+
+				on_load_values(*p_query);
+			}
+			m_db_op->step();
+		}
+		break;
+
+		case db::TransactionState::COMPLETED:
+			ATP_CORE_LOG(debug) << "Completed database transaction.";
+			on_finished();
+			m_done = true;
+			m_db_op.reset();
+			break;
+
+		case db::TransactionState::FAILED:
+			ATP_CORE_LOG(error) << "Database transaction failed, "
+				"query process bailing...";
+			on_failed();
+			m_done = true;
+			m_db_op.reset();
+			break;
+		}
+	}
+	else
+	{
+		ATP_CORE_LOG(debug) << "Building query for query "
+			"process...";
+
+		const auto qb = create_query();
+
+		ATP_CORE_LOG(debug) << "Obtained query builder.";
+
+		const auto q = qb->build();
+
+		ATP_CORE_LOG(debug) << "Built query \"" << q << "\".";
+
+		m_db_op = m_db->begin_transaction(q);
+
+		if (m_db_op != nullptr)
+		{
+			ATP_CORE_LOG(debug) << "Successfully built query!";
+		}
+		else
+		{
+			ATP_CORE_LOG(error) << "Failed to build query \""
+				<< q << "\", query process terminating...";
+			m_done = true;
+		}
+	}
+}
+
+
+}  // namespace core
+}  // namespace atp
+
+
---
+++ ATPCore/Processes/QueryProcess.h
@@ -0,0 +1,65 @@
+#pragma once
+
+
+/**
+\file
+
+\author Samuel Barrett
+
+\brief Contains a helper class for writing processes which are
+	centred around running a query.
+*/
+
+
+#include <ATPDatabase.h>
+#include "../ATPCoreAPI.h"
+#include "IProcess.h"
+
+
+namespace atp
+{
+namespace core
+{
+
+
+/**
+\brief This process implements the IProcess virtual functions in
+	terms of other virtual functions which are centred around the
+	actual database operations that are ongoing, making it easier
+	to implement such a process.
+*/
+class ATP_CORE_API QueryProcess :
+	public IProcess
+{
+public:
+	QueryProcess(const db::DatabasePtr& p_db);
+
+	inline bool done() const override
+	{
+		return m_done;
+	}
+	inline bool waiting() const override
+	{
+		return m_db_op != nullptr &&
+			m_db_op->waiting();
+	}
+	void run_step() override;
+
+protected:  // implement these functions!
+
+	virtual std::unique_ptr<db::IQueryBuilder> create_query() = 0;
+	virtual void on_load_values(db::IQueryTransaction& query) { }
+	virtual void on_finished() { }
+	virtual void on_failed() { }
+
+private:
+	db::DatabasePtr m_db;
+	db::TransactionPtr m_db_op;
+	bool m_done;
+};
+
+
+}  // namespace core
+}  // namespace atp
+
+

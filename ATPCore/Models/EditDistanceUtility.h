8e77dc37b4da6d51f916eb990b6d67660a05310a
Sam Barrett <samuelbarrett1234@btinternet.com>
2020-05-10 11:34:00 +0100

    Created first version of edit distance implementation.

---
+++ ATPCore/Models/EditDistanceUtility.h
@@ -0,0 +1,101 @@
+#pragma once
+
+
+/**
+\file
+
+\author Samuel Barrett
+
+*/
+
+
+#include <map>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <ATPLogic.h>
+#include "../ATPCoreAPI.h"
+
+
+namespace atp
+{
+namespace core
+{
+
+
+/**
+\brief Used for storing the costs of substituting two symbols in the
+	edit distance algorithm
+
+\details A mapping from (symbol ID 1, symbol ID 2) to cost, where the
+	cost represents the cost of substituting 1 -> 2
+
+\invariant The arity of symbol 1 is at least the arity of symbol 2
+	(this is because substitutions which increase the arity are kind
+	of ambiguous - what would the distances of the new arguments be?)
+
+\invariant This mapping must contain, for EVERY pair of symbols (a,b)
+	such that b's arity <= a's arity, a floating point value. Note
+	that for consistent results, you should set (a,a) to have cost 0
+	for every symbol a.
+*/
+typedef std::map<std::pair<size_t, size_t>, float> EditDistSubCosts;
+
+
+/**
+\brief Given a matrix of shape (N,M) where N >= M find a sequence of
+	M integers x_i in the range 0..N-1 such that the sum of distances
+	(x_i, i) is minimised.
+
+\details Given a pairwise distances matrix, this computes a good
+	subset and permutation of the elements in this matrix which
+	minimises the sum cost. This is equivalent to finding the minimum
+	cost matching.
+
+\note The size of these matrices is bounded (i.e. O(1)) when given a
+	fixed model context, so this algorithm is technically constant
+	time w.r.t. the size of the statements we're computing the edit
+	distance for.
+
+\pre distances.size1() >= distances.size2()
+
+\returns The minimum cost, as described above.
+*/
+ATP_CORE_API float minimum_assignment(
+	boost::numeric::ublas::matrix<float>& distances);
+
+
+/**
+\brief Compute the edit distance between two logical statements
+
+\param sub_costs The cost of each symbol substitution.
+
+\pre See requirements about the EditDistSubCosts mapping.
+*/
+ATP_CORE_API float edit_distance(
+	const logic::IStatement& stmt1,
+	const logic::IStatement& stmt2,
+	const EditDistSubCosts& sub_costs);
+
+
+/**
+\brief Compute the edit distance between two pairs of arrays.
+
+\details This is strictly more efficient than calling edit_distance
+	on each pair.
+
+\pre See requirements about the EditDistSubCosts mapping.
+
+\pre stmtarr1.size() == stmtarr2.size()
+
+\returns An array A such that A[i] = edit_distance(stmtarr1.at(i),
+	stmtarr2.at(i))
+*/
+ATP_CORE_API std::vector<float> pairwise_edit_distance(
+	const logic::IStatementArray& stmtarr1,
+	const logic::IStatementArray& stmtarr2,
+	const EditDistSubCosts& sub_costs);
+
+
+}  // namespace core
+}  // namespace atp
+
+
---
+++ ATPCoreTests/EditDistanceUtilityTests.cpp
@@ -0,0 +1,130 @@
+/**
+\file
+
+\author Samuel Barrett
+
+\brief Test suite for basic statement edit distance functionality.
+
+*/
+
+
+#include <Models/EditDistanceUtility.h>
+#include "Test.h"
+#include "LogicSetupFixture.h"
+
+
+using atp::core::minimum_assignment;
+using atp::core::edit_distance;
+using atp::core::pairwise_edit_distance;
+using atp::core::EditDistSubCosts;
+namespace ublas = boost::numeric::ublas;
+using atp::logic::StmtFormat;
+
+
+struct EditDistanceTestsFixture :
+	public LogicSetupFixture
+{
+	EditDistanceTestsFixture()
+	{
+		// setup costs
+
+		sub_costs[std::make_pair(
+			p_ctx->symbol_id("e"), p_ctx->symbol_id("e")
+		)] = 0.0f;
+
+		sub_costs[std::make_pair(
+			p_ctx->symbol_id("i"), p_ctx->symbol_id("e")
+		)] = 10.0f;
+
+		sub_costs[std::make_pair(
+			p_ctx->symbol_id("i"), p_ctx->symbol_id("i")
+		)] = 0.0f;
+
+		sub_costs[std::make_pair(
+			p_ctx->symbol_id("*"), p_ctx->symbol_id("e")
+		)] = 10.0f;
+
+		sub_costs[std::make_pair(
+			p_ctx->symbol_id("*"), p_ctx->symbol_id("i")
+		)] = 10.0f;
+
+		sub_costs[std::make_pair(
+			p_ctx->symbol_id("*"), p_ctx->symbol_id("*")
+		)] = 0.0f;
+	}
+
+	EditDistSubCosts sub_costs;
+};
+
+
+BOOST_FIXTURE_TEST_SUITE(EditDistanceTests,
+	EditDistanceTestsFixture);
+
+
+BOOST_AUTO_TEST_CASE(minimum_assignment_square_test)
+{
+	ublas::matrix<float> dists(3, 3);
+
+	dists(0, 0) = 33.0f; dists(0, 1) = 1.0f; dists(0, 2) = 10.0f;
+	dists(1, 0) = 1.0f; dists(1, 1) = 2.0f; dists(1, 2) = 5.0f;
+	dists(2, 0) = 10.0f; dists(2, 1) = 5.0f; dists(2, 2) = 6.0f;
+
+	BOOST_TEST(minimum_assignment(dists) == 16.0f);
+}
+
+
+BOOST_AUTO_TEST_CASE(minimum_assignment_not_square_test)
+{
+	ublas::matrix<float> dists(3, 2);
+
+	dists(0, 0) = 33.0f; dists(0, 1) = 1.0f;
+	dists(1, 0) = 1.0f; dists(1, 1) = 2.0f;
+	dists(2, 0) = 10.0f; dists(2, 1) = 5.0f;
+
+	BOOST_TEST(minimum_assignment(dists) == 7.0f);
+}
+
+
+BOOST_DATA_TEST_CASE(test_edit_dist,
+	boost::unit_test::data::make({
+		"x0 = x0",
+		"e = e",
+		"e = x0",
+		"e = e",
+		"i(x0) = e",
+		"e = e",
+		"*(x0, x1) = i(e)"
+		}) ^
+	boost::unit_test::data::make({
+		"x0 = x0",
+		"e = e",
+		"e = e",
+		"e = x0",
+		"e = e",
+		"i(x0) = e",
+		"*(x0, i(e)) = x0"
+		}) ^
+	boost::unit_test::data::make({
+		0.0f,
+		0.0f,
+		1.0f,
+		1.0f,
+		10.0f,
+		10.0f,
+		10.0f + 10.0f
+		}), stmt1_str, stmt2_str, target_dist)
+{
+	s << stmt1_str << "\n" << stmt2_str;
+
+	auto p_stmts = p_lang->deserialise_stmts(s, StmtFormat::TEXT,
+		*p_ctx);
+
+	float dist = edit_distance(p_stmts->at(0), p_stmts->at(1));
+
+	BOOST_TEST(dist = target_dist);
+}
+
+
+BOOST_AUTO_TEST_SUITE_END();
+
+

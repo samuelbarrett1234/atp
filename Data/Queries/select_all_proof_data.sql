07067f25f80dad02b47121b7e3a365e82c6a869e
Sam Barrett <samuelbarrett1234@btinternet.com>
2020-05-14 07:38:25 +0100

    Made some tweaks to search settings and queries.

--- Data/Queries/select_all_proof_data.sql
+++ Data/Queries/select_all_proof_data.sql
@@ -1,10 +1,12 @@
-SELECT stmt, num_attempts, num_proofs, time_agg, max_mem_agg, num_exp_agg,
+SELECT stmt, num_attempts, time_agg, max_mem_agg, num_exp_agg,
 
 -- compute the number of times this theorem could've been / was used in a proof
 IFNULL((SELECT SUM(cnt) FROM theorem_usage WHERE id=used_thm_id), 0) AS num_uses,
 
   -- convert date from julianday to something readable
-DATE(thm_date) as date_entered
+DATE(thm_date) AS date_entered,
+
+DATE(proof_date) AS date_proven
 
 FROM theorems JOIN
 
@@ -14,16 +16,10 @@ FROM theorems JOIN
 		SUM(num_expansions) AS num_exp_agg, COUNT(thm_id) AS num_attempts
 		FROM proof_attempts GROUP BY thm_id)
 		
-	NATURAL JOIN
-	
-	-- get whether or not each statement has been proven (num proofs will
-	-- always be 0 or 1)
-	(SELECT id AS thm_id,
-		(CASE thm_id IS NULL WHEN TRUE THEN 0 ELSE 1 END)
-		AS num_proofs FROM theorems LEFT OUTER JOIN proofs ON id=thm_id)
+	NATURAL JOIN proofs
 )
 
 ON id=thm_id
 
-ORDER BY num_proofs DESC, num_uses DESC, num_attempts DESC
+ORDER BY num_uses DESC, num_attempts DESC
 ;

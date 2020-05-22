ef7e7c40699bf33070934d296233358446c57112
Sam Barrett <samuelbarrett1234@btinternet.com>
2020-05-22 13:38:28 +0100

    Created some Python scripts to experiment with ML setups.

---
+++ Scripts/convert_data.py
@@ -0,0 +1,23 @@
+import sqlite3 as sql
+import pandas as pd
+
+db = sql.connect("../Data/DB/eqlogic.db")
+
+# this script is for converting all theorem data for a given context to a
+# CSV file
+
+ctx = "group-theory"
+
+df = pd.read_sql_query("SELECT stmt, "
+    "(CASE WHEN proof IS NULL THEN 0 ELSE 1 END) AS is_proven, "
+    "time_agg, max_mem_agg, num_exp_agg, num_attempts "
+    "FROM theorems LEFT OUTER NATURAL JOIN proofs NATURAL JOIN "
+    "(SELECT thm_id, SUM(time_cost) AS time_agg, MAX(max_mem) AS max_mem_agg, "
+	"SUM(num_expansions) AS num_exp_agg, COUNT(thm_id) AS num_attempts "
+	"FROM proof_attempts GROUP BY thm_id) NATURAL JOIN model_contexts "
+    "WHERE name = '" + ctx + "'", db)
+
+db.close()
+
+df.to_csv("../Data/DB/thms.csv", index=False)
+
